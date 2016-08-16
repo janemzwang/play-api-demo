427de765373ca87d1a703b03e0898d8b4677b5c1
Kang Kang Wang <kangwang1982@gmail.com>
2016-08-15 22:26:41 -0400

    simplify demo

--- app/controllers/PropertyController.java
+++ app/controllers/PropertyController.java
@@ -1,20 +1,47 @@
 package controllers;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import models.BaseModel;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import dao.DaoFactory;
 import models.Property;
+import play.db.jpa.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.List;
 
 /**
  * @author Kang Kang Wang
  */
-public class PropertyController extends BaseController {
+public class PropertyController extends Controller {
   public PropertyController() {
-    this.injector = Guice.createInjector(new AbstractModule() {
-      @Override
-      protected void configure() {
-        bind(BaseModel.class).to(Property.class);
-      }
-    });
+  }
+
+  @Transactional
+  public Result getById(int id) {
+    Property object = DaoFactory.getPropertyDAO().findById(id);
+
+    if (object == null) {
+      return notFound();
+    }
+
+    String jsonResponse = jsonifyResponse(object);
+
+    return ok(jsonResponse);
+  }
+
+  @Transactional
+  public Result getAll() {
+    List<Property> properties = DaoFactory.getPropertyDAO().findAll();
+    String jsonResponse = jsonifyResponse(properties);
+    return ok(jsonResponse);
+  }
+
+  private String jsonifyResponse(Object obj) {
+    Gson gson = new GsonBuilder().setVersion(1.0)
+        .setPrettyPrinting().serializeNulls().create();
+    JsonElement json = gson.toJsonTree(obj);
+    return gson.toJson(json);
   }
 }
