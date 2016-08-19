ed5978c0d2db95f76e84960d65d5b21b70c04a26
Kang Wang <kawang@linkedin.com>
2016-08-19 13:29:42 -0400

    add JDBC sample code

--- app/controllers/PropertyController.java
+++ app/controllers/PropertyController.java
@@ -5,17 +5,44 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import dao.DaoFactory;
 import models.Property;
+import play.db.Database;
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 /**
  * @author Kang Kang Wang
  */
 public class PropertyController extends Controller {
-  public PropertyController() {
+  private Database db;
+
+  @Inject
+  public PropertyController(Database db) {
+    this.db = db;
+  }
+
+  private Property findProperty(int id) throws SQLException {
+    try(Connection conn = db.getConnection();
+      Statement st = conn.createStatement()) {
+      ResultSet rs = st.executeQuery("select * from property where id =" + id);
+      Property property = new Property();
+      while(rs.next()) {
+        property.id = Long.valueOf(id);
+        property.mlsId = rs.getLong("mlsId");
+        property.buildYear = rs.getInt("build_year");
+        property.listingPrice = rs.getInt("listing_price");
+        property.marketStatus = rs.getString("market_status");
+        //and so on....
+      }
+      return property;
+    }
   }
 
   @Transactional
